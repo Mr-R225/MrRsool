f0e381d27f401152ee345225a63a3fffe681038e
Mr-R225 <39289908+Mr-R225@users.noreply.github.com>
2018-06-23 10:13:17 +0700

    MrRsool

---
+++ MrRsool.sh
@@ -0,0 +1,549 @@
+#usr/xbin/sh
+#yg recode tool saya semoga masuk neraka 
+#aminnnn
+
+
+
+pesan() {
+clear
+echo "pesan:Bagi siapa yg recode tool ane semoga masuk neraka"
+echo "pesan2:Thanks for install my tool :)"
+selection
+}
+thanks() {
+clear
+echo "Thanks"
+echo "Amriez && Google && Mr.IM81		"
+selection
+}
+whatsapp() {
+clear
+echo "contact me :+6285231531200"
+selection
+}
+about() {
+	clear
+	echo "      Author:Mr.R						 "
+	echo "      Code:Bash						 "
+	echo "      Made on date:22 Juni 2018		 "
+	echo "      Github:https://github.com/Mr-R225"
+	echo "      Whatsapp:+6285231531200			 "
+	echo "       + Thanks To +         			 "
+	echo "          Google             			 "
+	selection
+}
+quit() {
+	clear
+	echo "Byeee:)"
+	exit
+}
+restartprogram() {
+	echo "Byee:)"
+	exit
+}
+
+selection(){
+	echo -e "1~>Pesan dari Mr.R"
+	echo -e "2~>Thanks To"
+	echo -e "3~>Contact Me :)"
+	echo -e "99~>About"
+	echo -e "0~>Quit"
+	echo ""
+	read -p "#MrRsool~>#" selection
+
+	if [ $selection = 1 ];
+	then
+	pesan
+	elif [ $selection = 2 ];
+	then
+	thanks 
+	elif [ $selection = 3 ];
+	then
+	whatsapp
+	elif [ $selection = 99 ];
+	then
+	about
+	elif [ $selection = 0 ];
+	then
+	quit
+	else
+		restartprogram
+	fi
+}
+
+
+
+lazada() {
+	cd module
+	python2 lazada.py
+}
+grab() {
+	cd module
+	python2 spammer.py
+}
+tokped() {
+	cd module
+	php tokped.php
+}
+telkom() {
+	cd module
+	php telkomsel.php
+}
+phd() {
+	cd module
+	php phd.php
+}
+jdid() {
+	cd module
+	php jdid.php
+}
+call() {
+	clear
+	spam
+	cd module
+	php run.php
+}
+quit() {
+	echo "bye-bye :)"
+}
+restartprogram() {
+	clear
+}
+
+tool(){
+	echo "1~>Spammer email lazada"
+	echo "2~>Spammer-Grab"
+	echo "3~>Spammer Tokped"
+	echo "4~>Spammer Telkomsel"
+	echo "5~>Spammer PHD"
+	echo "6~>Spammer Jd.Id"
+	echo "7~>Spammer call tokped"
+	echo "0~>Quit"
+	read -p "#Root@MrR~>#" spam
+
+	if [ $spam = 1 ];
+	then
+	lazada
+	elif [ $spam = 2 ];
+	then
+	grab
+	elif [ $spam = 3 ];
+	then
+	tokped
+	elif [ $spam = 4 ];
+	then
+	telkom
+	elif [ $spam = 5 ];
+	then
+	phd
+	elif [ $spam = 6 ];
+	then
+	jdid
+	elif [ $spam = 7 ];
+	then
+	call
+	elif [ $spam = 0 ];
+	then
+	quit
+	else 
+		restartprogram
+	fi
+
+}
+
+
+lite() {
+clear
+figlet "Mr.R" | lolcat
+apt update && apt upgrade
+pkg install git python python2 
+git clone https://github.com/4L13199/LITEDDOS
+cd LITEDDOS
+python2 LITEDDOS.py
+}
+ijo() {
+clear
+figlet "Mr.R" | lolcat
+apt update && apt upgrade
+pkg install git python python2
+git clone https://github.com/Amriez/GreenReaper
+cd GreenReaper
+python2 greenreper.py
+}
+palu() {
+clear
+figlet "Mr.R" | lolcat
+apt update && apt upgrade
+pkg install git python python2 python3
+git clone https://github.com/cyweb/hammer
+cd hammer
+python3 hammer.py
+}
+air() {
+clear
+figlet "Mr.R" | lolcat
+echo "pesan dari Mr.R !!! : (ex:python2 waterdos.py -t https://github.com 
+								-r 256 -p 80)"
+sleep 3
+apt update && apt upgrade
+pkg install python python2 git
+git clone https://github.com/JeyZeta/WaterDoS
+cd WaterDoS
+python2 waterdos.py 
+}
+xerxes() {
+figlet "Mr.R" | lolcat
+apt update && apt upgrade
+pkg install git clang
+git clone https://github.com/zanyarjamal/xerxes
+cd xerxes
+clang xerxes.c -o xerxes
+echo "ex ddos via xerxes (ex:./xerxes www.google.com 80)"
+}
+quit(){
+echo "\033[034;1m "
+echo "Bye-Bye and back again in my tool :)" 
+}
+restartprogram() {
+	clear
+}
+
+
+wazzap(){
+	echo -e "1~>LITEDDOS"
+	echo -e "2~>Green Reaper"
+	echo -e "3~>Hammer"
+	echo -e "4~>Water Dos"
+	echo -e "5~>Xerxes"
+	echo -e "0~>Quit"
+	echo ""
+	read -p "Root@MrR" ddos
+
+	if [ $ddos = 1 ];then
+	lite
+	elif [ $ddos = 2 ];then
+	ijo
+	elif [ $ddos = 3 ];then
+	palu
+	elif [ $ddos = 4 ];then
+	air
+	elif [ $ddos = 5 ];then
+	xerxes
+	elif [ $ddos = 0 ];then
+	quit
+	else
+		restartprogram
+	fi
+	
+}
+
+
+spam() {
+apt update && apt upgrade
+pkg install git toilet figlet 
+git clone https://github.com/Mr-R225/MrRSpam
+cd MrRSpam
+sh MrRSpam2.sh
+}
+tool() {
+apt update && apt upgrade
+pkg install git 
+git clone https://github.com/4L13199/LITETOOLS
+cd LITETOOLS
+chmod +x LITETOOLS.isl
+sh LITETOOLS.isl
+}
+lite() {
+apt update && apt upgrade
+pkg install git php toilet
+git clone https://github.com/4L13199/LITESPAM
+cd LITESPAM
+sh LITESPAM.sh
+}
+mr() {
+apt update && apt upgrade
+pkg install git toilet
+git clone https://github.com/Mr-R225/Mr.Rv1.2
+cd Mr.Rv1.2
+sh Mr.Rv1.2.sh
+}
+mbf() {
+apt update && apt upgrade
+pkg install git python python2 
+pip2 install mechanize
+git cline https://github.com/pirmansx/mbf
+cd mbf
+python2 MBF.py
+}
+admin() {
+apt uodate && apt upgrade
+pkg install git python python2
+git clone https://github.com/Techzindia/admin_penal
+cd admin_penal
+python2 admin_panel_finder.py
+}
+nmap() {
+apt update && apt upgrade
+pkg install nmap
+}
+red() {
+apt update && apt upgrade
+pkg install git php php-curl php-xml
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+cd RED_HAWK
+php rhawk.php 
+}
+weeman() {
+apt update && apt upgrade
+pkg install git python python2
+git clone https://github.com/evait-security/weeman.git
+cd weeman
+chmod +x weeman.py
+python2 weeman.py
+}
+dtect() {
+apt update && apt upgrade
+pkg install git python python2
+git clone https://github.com/shawarkhanethicalhacker/D-TECT
+cd D-TECT
+chmod +x d-tect.py
+python2 d-tect.py
+}
+ip() {
+apt uodate && apt upgrade
+pkg install git python python2
+git clone https://github.com/maldevel/IPGeolocation
+cd IPGelocation
+mv ipgeolocation.py main.py
+chmod +x main.py
+pip install -r requirements.txt
+./main.py-h
+}
+ohmyzsh() {
+apt update && apt upgrade
+pkg install nano
+sh -c "$(curl -fsSL https://github.com/Cabbagec/termux-ohmyzsh/raw/master/install.sh)"
+~/.termux/colors.sh
+~/.termux/fonts.sh
+}
+santet() {
+apt update
+apt upgrade
+pkg install python
+pkg install git
+git clone https://github.com/Gameye98/santet-online
+cd santet-online
+python santet.py
+}
+fatrat() {
+apt update && apt upgrade
+pkg install git
+git clone https://github.com/Screetsec/TheFatRat.git
+cd TheFatRat
+chmod +x fatrat ; chmod +x powerfull.sh
+./fatrat
+}
+sudo() {
+apt update && apt upgrade
+apt install git
+git clone https://github.com/st42/termux-sudo
+cd termux-sudo
+apt install ncurser-utils
+cat sudo > /data/data/com.termux/files/usr/bin/sudo
+chmod 700 /data/data/com.termux/files/usr/bin/sudo
+echo "untuk menjalan kan sudo tinggal ketik sudo di termux" | lolcat
+}
+breacher() {
+apt update && apt upgrade
+pkg install git
+pkg install python2
+git clone https://github.com/UltimateHackers/Breacher
+cd Breacher
+python2 breacher.py
+}
+irssi() {
+apt update && apt upgrade
+pkg install irsii
+irsii
+}
+sdc() {
+apt update
+apt upgrade
+apt install git
+apt install python
+apt install python2
+git clone https://github.com/Ubaii/script-deface-creator
+cd script-deface-creator
+python2 create.py
+}
+ngrok() {
+apt update && apt upgrade
+apt install wget
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip
+unzip ngrok-stable-linux-arm.zip
+}
+vbug() {
+apt update && apt upgrade -y
+pkg install wget
+apt install python2
+wget -O vbug.zip https://doc-0s-0o-docs.googleusercontent.com/docs/securesc/ha0ro937gc
+unzip vbug.zip
+cd vbug
+python2 vbug.py
+}
+router() {
+apt update && apt upgrade
+apt install git
+apt install python
+apt install python2
+pip2 install requests
+git clone https://github.com/reverse-shell/routersploit.git
+cd routersploit
+pip install -r requirements.txt termux-fix-shebang rsf.py
+cd routersploit
+./rsf.py
+}
+inmux() {
+apt update && apt upgrade
+pkg install git wget
+git clone https://github.com/Amriez/inmux
+cd inmux
+sh inmux.sh
+}
+aoc() { 
+apt update && apt upgrade
+pkg install git python python2
+git clone https://github.com/Amriez/AOCDEFACE
+cd AOCDEFACE
+bash AOC.sh
+}
+wpscan() { 
+apt update && apt upgrade
+apt install git
+apt install ruby
+git clone https://github.com/wpscanteam/wpscan/
+cd wpscan
+chmod 777 wpscan.rb
+gem install bundle
+bundle install -j5
+ruby wpscan.rb
+}
+quit() { 
+	echo "byee:)" | lolcat
+}
+restartprogram() {
+	clear
+}
+
+guys(){
+	echo -e "1~>Install MrRSpam"
+	echo -e "2~>Install LITETOOLS"
+	echo -e "3~>Install LITESPAM"
+	echo -e "4~>Install Mr.Rv1.2"
+	echo -e "5~>Install Webdav"
+	echo -e "5~>Install MBFv2"
+	echo -e "6~>Install Admin finder"
+	echo -e "7~>Install Nmap"
+	echo -e "8~>Install Red_HAWK"
+	echo -e "9~>Install Weeman"
+	echo -e "10~>Install D-Tect"
+	echo -e "11~>Install IPGeolocation"
+	echo -e "12~>Install oh my zsh"
+	echo -e "13~>Install santet-online"
+	echo -e "14~>Install Fat Rat"
+	echo -e "15~>Install sudo"
+	echo -e "16~>Install Breacher"
+	echo -e "17~>Install irssi"
+	echo -e "18~>Install Install Script Deface Creator"
+	echo -e "19~>Install ngrok"
+	echo -e "20~>Install VbugMaker"
+	echo -e "21~>Install RouterSploit"
+	echo -e "22~>Install infoga"
+	echo -e "23~>Install inmux"
+	echo -e "24~>Install AOCDeface"
+	echo -e "25~>Install WPScan"
+	echo -e "0~>Quit"
+	echo " "
+	read -p "Root@Mr.R~>#" instal
+
+	if [ $instal = 1 ];then
+	spam
+	elif [ $instal = 2 ];then
+	tool
+	elif [ $instal = 3 ];then
+	lite
+	elif [ $instal = 4 ];then
+	mr
+	elif [ $instal = 5 ];then
+	mbf
+	elif [ $instal = 6 ];then
+	admin
+	elif [ $instal = 7 ];then
+	nmap
+	elif [ $instal = 8 ];then
+	red
+	elif [ $instal = 9 ];then
+	weeman
+	elif [ $instal = 10 ];then
+	dtect
+	elif [ $instal = 11 ];then
+	ip
+	elif [ $instal = 12 ];then
+	ohmyzsh
+	elif [ $instal = 13 ];then
+	santet
+	elif [ $instal = 14 ];then
+	fatrat
+	elif [ $instal = 15 ];then
+	sudo
+	elif [ $instal = 16 ];then
+	breacher
+	elif [ $instal = 17 ];then
+	irssi
+	elif [ $instal = 18 ];then
+	sdc
+	elif [ $instal = 19 ];then
+	ngrok
+	elif [ $instal = 20 ];then
+	vbug
+	elif [ $instal = 21 ];then
+	router
+	elif [ $instal = 22 ];then
+	infoga
+	elif [ $instal = 23 ];then
+	inmux
+	elif [ $instal = 24 ];then
+	aoc
+	elif [ $instal = 25 ];then
+	wpscan
+	elif [ $instal = 0 ];then
+	quit
+	else
+		restartprogram
+	fi
+	
+}
+
+
+
+main(){
+echo -e "~{1}Information"
+echo -e "~{2}Tool Spammer"
+echo -e "~{3}Tool DDos"
+echo -e "~{4}Tool Installer"
+read -p ">~# " xxx
+
+	if [ $xxx = 1 ];then
+	selection
+	elif [ $xxx = 2 ];then
+	tool
+	elif [ $xxx = 3 ];then
+	wazzap
+	elif [ $xxx = 4 ];then
+	guys
+	else
+		restartprogram
+	fi
+	
+}
+main
